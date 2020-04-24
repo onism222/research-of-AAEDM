869cfb443669330191365673f8a538950523f1d5
KE"KEN"WANG <61583324+ken0225@users.noreply.github.com>
2020-04-24 20:56:50 +0800

    Update korea_kappa_zeta_312_321_interval_21days.m

--- korea_kappa_zeta_312_321_interval_21days.m
+++ korea_kappa_zeta_312_321_interval_21days.m
@@ -2,19 +2,19 @@ clear;
 clc;
 close all; 
 
-real_delta = xlsread('Covid19.xlsx',1, 'E27 : E57'); % real daily increase number 2.21 - 3.21
+real_delta = xlsread('Covid19.xlsx',1, 'E27 : E57'); % real daily increase number 2.20 - 3.21
 real_delta = reshape(real_delta, 1, length(real_delta));
 
-real_C = xlsread('Covid19.xlsx',1, 'G27 : G57'); % real daily cured number 2.21 - 3.21
+real_C = xlsread('Covid19.xlsx',1, 'G27 : G57'); % real daily cured number 2.20 - 3.21
 real_C = reshape(real_C, 1, length(real_C));
 
-n = 10; % length(real_delta) == length(real_C)
+n = 10; % we just want to obtain the kappa and zeta from 3.12 to 3.21
 kappa_seq = zeros(1, n);
 zeta_seq = zeros(1, n);
 
 for k = 1 :  n
     
-kappa = kappa_factor(real_C(k : 20+k), real_delta(k : 20+k)); % equation (2.5)
+kappa = kappa_factor(real_C(k : 20 + k), real_delta(k : 20+k)); % equation (2.5)
 kappa_seq(k) = kappa;
 disp(['In the ', num2str(k),' th day, the kappa factor is ', num2str(kappa)]);
 zeta = zeta_factor(real_delta(k : 20+k)); % equation (2.7)
